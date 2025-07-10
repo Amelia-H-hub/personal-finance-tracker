c5407b6eddd5a4924905a3c7faffcf1ee508798a
Yuki Okabe <s.n.w20240703@gmail.com>
2025-07-10 11:11:41 -0700

    Edited class Analysis and class Transaction

--- analysis.py
+++ analysis.py
@@ -2,6 +2,9 @@
 
 class Analysis:
     def analyze_spending_category(self, df):
+        if df.empty:
+            return df
+        
         print("Analyze Spending Category")
         print("--- Total Spending by Category ---")
 
@@ -9,10 +12,13 @@ class Analysis:
         print(total_spending_by_category.to_string())
 
     def calculate_average_monthly_spending(self, df):
+        if df.empty:
+            return df
+        
         print("Calculate Average Monthly Spending")
         print("--- Average Monthly Spending ---")
 
-        avg = round(df[df["Type"] == "Expense"]["Amount"].mean(), 2)
+        avg = round(df[df["Type"] == "Expense"].groupby(df["Date"].dt.to_period('M'))["Amount"].mean().mean(), 2)
         print(avg)
 
     def show_top_spending_category(self, df):
